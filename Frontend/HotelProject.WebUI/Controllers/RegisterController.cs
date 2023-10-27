9a819d68d3edc414c512e5a8253fe79cc7baa675
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-28 00:00:51 +0300

    Identity Library , Register and Login added.
    Iddentity Kütüphanelerimizi ekledik.
    Register ve Login işlemleri için Controllerlarımızı ve sınıflarımızı ekledik.
    Index sayfalarımızı oluşturduk.
    Mapping işlemlerimizi yaptık.

---
+++ Frontend/HotelProject.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.RegisterDto;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult RegisterIndex()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RegisterIndex(CreateNewUserDto createNewUserDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var appUser = new AppUser()
+            {
+                Name = createNewUserDto.Name,
+                Email = createNewUserDto.Mail,
+                Surname = createNewUserDto.Surname,
+                UserName = createNewUserDto.Username
+            };
+            var result = await _userManager.CreateAsync(appUser, createNewUserDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("LoginIndex", "Login");
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage = "Ad alanı gereklidir!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad alanı gereklidir!")]
+        public string Surname { get; set; }
+
+        //[Required(ErrorMessage = "Şehir alanı gereklidir!")]
+        //public string City { get; set; }
+
+        [Required(ErrorMessage = "Kullanıcı Adı alanı gereklidir!")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail alanı gereklidir!")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanı gereklidir!")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Şifre tekrar alanı gereklidir!")]
+        [Compare("Password", ErrorMessage = "Şifreler uyuşmuyor!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
