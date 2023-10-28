675d6250e85b524088c0197cb40142f6ad858a03
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-29 00:49:44 +0300

    AdminBooking Added.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminBookingController.cs
@@ -0,0 +1,44 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminBookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminBookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> AdminBookingIndex()
+        {
+            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
+            var responseMessage = await client.GetAsync("http://localhost:62391/api/Booking");          // Belirtilen adrese istekte bulunuldu.
+            if (responseMessage.IsSuccessStatusCode)                                                 // Adresten başarılı durum kodu dönerse ,
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
+                return View(values);
+            }
+            return View();
+        }
+        public async Task<IActionResult> ApprovedReservation(ApprovedReservationDto approvedReservationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(approvedReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:62391/api/Booking/bbbb", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("AdminBookingIndex");
+            }
+            return View();
+        }
+    }
+}
