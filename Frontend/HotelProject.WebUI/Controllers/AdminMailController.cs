00a05de3762303ccb1e2b515c285e9400d91b142
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-30 23:57:15 +0300

    Mail Gönderme işlemi.
    Google üzerinden mail gönderme işlemi yapıldı.

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,43 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminMailController : Controller
+    {
+        [HttpGet]
+        public IActionResult AdminMailIndex()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AdminMailIndex(AdminMailViewModel adminMailViewModel)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+
+            MailboxAddress mailboxAdressFrom = new MailboxAddress("HotelrothAdmin", "kadiraduriz72@gmail.com");          //Mailin kimden olacağı
+            mimeMessage.From.Add(mailboxAdressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", adminMailViewModel.ReceiverMail);               // Kime olacağı
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = adminMailViewModel.Body;                                                            //Mesajımızın içeriği.
+            mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+            mimeMessage.Subject = adminMailViewModel.Subject;                                                         // Başlığımız
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("kadiraduriz72@gmail.com", "dyyufbttgzarkgsu");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+
+            // Gönderilen Mailin Veri Tabanına Kaydedilmesi.
+            return View();
+        }
+    }
+}
