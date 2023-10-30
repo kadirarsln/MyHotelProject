90ae64c7c52364d2ecdca4e00d5dfa3877a40974
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-30 16:19:55 +0300

    update guest

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/GuestManager.cs
@@ -0,0 +1,46 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class GuestManager : IGuestService
+    {
+        private readonly IGuestDal _guestDal;
+
+        public GuestManager(IGuestDal guestDal)
+        {
+            _guestDal = guestDal;
+        }
+
+        public void TDelete(Guest delete)
+        {
+            _guestDal.Delete(delete);
+        }
+
+        public Guest TGetByID(int id)
+        {
+            return _guestDal.GetByID(id);
+        }
+
+        public List<Guest> TGetList()
+        {
+            return _guestDal.GetList();
+        }
+
+        public void TInsert(Guest insert)
+        {
+            _guestDal.Insert(insert);
+        }
+
+        public void TUpdate(Guest update)
+        {
+            _guestDal.Update(update);
+        }
+    }
+}
