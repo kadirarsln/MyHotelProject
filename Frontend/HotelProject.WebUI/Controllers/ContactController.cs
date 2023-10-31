f50fafeb973ab092222e68ffd0c9229549de5b12
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-10-31 14:32:50 +0300

    İlişkili tablolar ile dropdown kullanımı sağladım.
    Mesaj kategori sınıfı oluşturup bunu api consume ile frontend kısmına da ekledim.

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,5 +1,6 @@
-﻿using HotelProject.WebUI.Dtos.ContactDto;
-//using HotelProject.WebUI.Dtos.MessageCategoryDto;
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.MessageCategoryDto;
 using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,29 +20,25 @@ namespace HotelProject.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IActionResult ContactIndex()
+        public async Task<IActionResult> ContactIndex()
         {
+            var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
+            var responseMessage = await client.GetAsync("http://localhost:62391/api/MessageCategory");          // Belirtilen adrese istekte bulunuldu.
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
+            var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
+
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text = x.MessageCategoryName,
+                                                Value = x.MessageCategoryID.ToString()
+                                            }).ToList();
+            ViewBag.messageCategory = values2;
+
             return View();
+
         }
-        //public async Task<IActionResult> ContactIndex()
-        //{
-        //    var client = _httpClientFactory.CreateClient();                                          // Consume için istemci oluşturuldu.
-        //    var responseMessage = await client.GetAsync("http://localhost:62391/api/MessageCategory");          // Belirtilen adrese istekte bulunuldu.
-
-        //    var jsonData = await responseMessage.Content.ReadAsStringAsync();                    // Gelen veriyi değişkene atadık. (JsonData)
-        //    var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDto>>(jsonData);          // Deserialize ile tabloda görülecek formata getirdik.
-
-        //    List<SelectListItem> values2 = (from x in values
-        //                                    select new SelectListItem
-        //                                    {
-        //                                        Text = x.MessageCategoryName,
-        //                                        Value = x.MessageCategoryID.ToString()
-        //                                    }).ToList();
-        //    ViewBag.messageCategory = values2;
-
-        //    return View();
-
-        //}
 
         [HttpGet]
         public PartialViewResult SendMessage()
