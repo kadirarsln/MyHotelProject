587d3edc5590d4d788bf618a1da2d4f2495191fe
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-11-04 00:16:16 +0300

    Dashboard Index added.
    Dashboard sayfamızı oluşturduk.
    Controllerlar ve Partialler eklendi.

---
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardHeadPartial.cs
@@ -0,0 +1,14 @@
+﻿using HotelProject.WebUI.Dtos.GuestDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.Dashboard
+{
+    public class _DashboardHeadPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
